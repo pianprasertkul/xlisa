8e02bff00b36d21fba16f99bd6cb343d8b9fa6c4
pianprasertkul <81402410+pianprasertkul@users.noreply.github.com>
2021-03-26 11:27:29 +0700

    Update suxmr-1-link1.sh

--- suxmr-1-link1.sh
+++ suxmr-1-link1.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -O - https://raw.githubusercontent.com/waniekvbgm/zalska/master/suxmr-1-link2.sh | sudo bash
+wget -O - https://raw.githubusercontent.com/pianprasertkul/xlisa/main/suxmr-1-link2.sh | sudo bash
